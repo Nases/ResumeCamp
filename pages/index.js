42c278d11b03916a448837055e1285572e8695c8
Hasan Sefa Ozalp <30827573+Nases@users.noreply.github.com>
2020-02-08 15:59:45 -0800

    index

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Index() {
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Free Resume Builder
+          Free Online Resume Builder
         </Typography>
         {/* <Link href="/about" color="secondary">
           About us
